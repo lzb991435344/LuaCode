4605455a738be5f98e5a7ebd97a083e78d78ee46
lzb991435344 <991435344@qq.com>
2020-08-16 22:18:25 +0800

    coroutine, producer & consumer

---
+++ 01Grammer/07coroutine/03producer&consumer.lua
@@ -0,0 +1,31 @@
+local newProductor
+
+function productor()
+     local i = 0
+     while true do
+          i = i + 1
+          send(i)     -- 将生产的物品发送给消费者
+     end
+end
+
+function consumer()
+     while true do
+          local i = receive()     -- 从生产者那里得到物品
+          print(i)
+     end
+end
+
+function receive()
+     local status, value = coroutine.resume(newProductor)
+     return value
+end
+
+--resume和yield的配合强大之处在于，resume处于主程中，它将外部状态（数据）传入到协同程序内部；
+--而yield则将内部的状态（数据）返回到主程中。
+function send(x)
+     coroutine.yield(x)     -- x表示需要发送的值，值返回以后，就挂起该协同程序
+end
+
+-- 启动程序
+newProductor = coroutine.create(productor)
+consumer()
