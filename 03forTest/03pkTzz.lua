1453c97f9e82310929b6172a2ffed80b1cd17e3d
lzb991435344 <991435344@qq.com>
2020-09-13 12:59:56 +0800

    牌型特征值检测

---
+++ 03forTest/03pkTzz.lua
@@ -0,0 +1,30 @@
+
+local cards = {
+
+}
+
+
+
+
+function expoldCards(cards)
+	
+end
+
+print(true or true or true)
+print(false or false or true)
+print(false or true)
+print(true or false or false)
+
+
+function  test()
+	if 1 then 
+		local i = 2
+	end	
+
+	if 1 then
+		print(i)
+	end	
+
+end 
+
+test()
