b5d0dfed8d4e0dacd918f79c98d534eb3a52d4d1
廖志标 <liaozhibiao@dartou.com>
2020-11-13 23:42:22 +0800

    add:增加关于修改table中数据值两种方法的比较

--- 03forTest/08test02.lua
+++ 03forTest/08test02.lua
@@ -16,3 +16,33 @@ for i = 1,2 do
         print("flag==0",i,money)
     end
 end
+--[[
+flag==1 1       11
+flag==0 2       15
+]]--
+
+
+
+--关于table浅拷贝问题
+--修改表数据中的值,需要先用临时变量获取表的地址,再对里面的变量赋值
+local t ={money = 10}
+
+local temp = t.money
+
+temp = temp + 10
+
+
+print(temp) --20
+print(t.money) --10
+
+
+
+
+local y = t --y获取t表的地址
+y.money = y.money + 10
+
+print(t.money) --20
+
+
+
+
